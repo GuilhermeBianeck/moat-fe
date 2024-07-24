0e61a85517e7c258ba7d69732267d6753005acfd
GANDHIX <mattdixon2000@hotmail.com>
2024-07-24 19:06:24 +0100

    Fix leaderboard bug

--- src/LeaderBoard.js
+++ src/LeaderBoard.js
@@ -15,30 +15,36 @@ class LeaderBoard extends React.Component {
   render() {
     let i = 0;
 
+    let content;
+    if (this.props.leaderBoardLoading) {
+      content = <Loading />;
+    } else {
+      if (this.props.leaderBoard.length) {
+        content = this.props.leaderBoard.map((entry) => (
+          <div key={i++} className="LeaderBoardEntry">
+            <span className="LeaderBoardName">{entry.nickname}</span>
+            <span className="LeaderBoardScore">{entry.score}</span>
+          </div>
+        ));
+      } else {
+        content = <p className="LeaderBoardEmptyMsg">Leaderboard is empty</p>;
+      }
+    }
+
     return (
       <div
         className="PopUpContainer"
         onClick={(evt) => {
-          if (evt.target != evt.currentTarget) return;
+          if (evt.target != evt.currentTarget) {
+            return;
+          }
 
           this.props.showLeaderBoard(false);
         }}
       >
         <div className="LeaderBoard PopUp-Screen RoundBorder">
           <h2>Leaderboard</h2>
-
-          {this.props.leaderBoardLoading ? <Loading /> : null}
-
-          {!this.props.leaderBoardLoading && !this.props.leaderBoard.length ? (
-            <p className="LeaderBoardEmptyMsg">Leaderboard is empty</p>
-          ) : null}
-
-          {this.props.leaderBoard.map((entry) => (
-            <div key={i++} className="LeaderBoardEntry">
-              <span className="LeaderBoardName">{entry.nickname}</span>
-              <span className="LeaderBoardScore">{entry.score}</span>
-            </div>
-          ))}
+          {content}
         </div>
       </div>
     );
